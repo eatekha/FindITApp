f98163cc7773aa38a125da57cef6ff7a3042fc24
Eseosa <atekhaeseosa@gmail.com>
2023-11-13 08:06:09 -0800

    Initial Commit

---
+++ FindIT/FindITApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindITApp.swift
+//  FindIT
+//
+//  Created by Eseosa on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct FindITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
