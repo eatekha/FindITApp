db26554e1052f9b2a8656eed7211b8893c177bb8
Eseosa <atekhaeseosa@gmail.com>
2023-11-30 00:28:51 -0800

    Able to Manage WebSockets

--- FindIT/ContentView.swift
+++ FindIT/ContentView.swift
@@ -1,17 +1,12 @@
-//
-//  ContentView.swift
-//  FindIT
-//
-//  Created by Eseosa on 2023-11-29.
-//
-
 import SwiftUI
 import AVFoundation
 import Speech
-
+import SocketIO  // Make sure this is correctly imported
 
 struct ContentView: View {
     @StateObject private var speechRecognizerManager = SpeechRecognizerManager()
+    // Add a state to manage the WebSocket connection status
+    @State private var webSocketConnected = false
 
     var body: some View {
         ZStack {
@@ -25,6 +20,7 @@ struct ContentView: View {
                     .font(.subheadline)
                     .foregroundColor(.black)
 
+                // Mic button
                 Button(action: {
                     speechRecognizerManager.startRecording()
                 }) {
@@ -42,13 +38,55 @@ struct ContentView: View {
                         .font(.subheadline)
                         .foregroundColor(.red)
                 }
+
+                // WebSocket setup button
+               /* / Button(action: {
+                    sendMessageThroughWebSocket()
+                }) {
+                    Text("Send WebSocket Mesage")
+                        .font(.headline)
+                        .foregroundColor(webSocketConnected ? .black : .blue)
+                        .padding()
+                        .background(webSocketConnected ? Color.gray.opacity(0.5) : Color.blue.opacity(0.5))
+                        .cornerRadius(10)
+                }
+                */
             }
         }
+        /*
+        .onAppear {
+            setupWebSocketConnection()
+        }
+         */
+
     }
+    
+    
+    private func setupWebSocketConnection() {
+            if !webSocketConnected {
+                WebSocketManager.shared.establishConnection()
+                webSocketConnected = true
+                
+                // Set up listener here if it's general and not specific to an event
+                WebSocketManager.shared.listenToEvent(event: "message") { data in
+                    print("Received response from message: \(data)")
+                }
+            }
+        }
+
+        private func sendMessageThroughWebSocket() {
+            // Send message
+            WebSocketManager.shared.sendMessage(message: "HI", to: "message")
+            print("Sent 'HI' to message on WebSocket")
+        }
+    
+    private func closeConnection(){
+        WebSocketManager.shared.closeConnection()
+    }
+    
 }
 
 // Preview provider for ContentView
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- FindIT/SpeechRecognizerManager.swift
+++ FindIT/SpeechRecognizerManager.swift
@@ -1,24 +1,51 @@
 import Speech
 
+
+
 class SpeechRecognizerManager: ObservableObject {
+    // Speech recognizer for converting speech to text.
     private let speechRecognizer = SFSpeechRecognizer()
+    
+    // Audio engine for managing and processing audio.
     private var audioEngine = AVAudioEngine()
+    
+    // Request for speech recognition.
     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    // Task for managing the speech recognition process.
     private var recognitionTask: SFSpeechRecognitionTask?
     
-    let silenceThreshold: Float = -70 // dB, adjust based on testing
+    //Managing the Recording of Audio
+    private var audioRecorder: AVAudioRecorder?
+    
+    //Managing the Playing of Audio
+    private var audioPlayer: AVAudioPlayer?
+
+    
+    // Threshold for detecting silence in decibels.
+    let silenceThreshold: Float = -60 // Adjust based on testing
+    
+    // Timer for detecting a duration of silence.
     private var silenceTimer: Timer?
+    
+    // Flag to indicate if silence is detected.
     private var isSilenceDetected = false
     
+    // Published properties to observe changes in the UI.
     @Published var transcribedText: String = ""
+    var temp: String = ""
+
     @Published var isRecording: Bool = false
     
+    // Starts the recording and speech recognition process.
     func startRecording() {
+        // If already recording, stop and return.
         if isRecording {
             stopRecording()
             return
         }
         
+        // Set recording flag to true and prepare for a new recognition request.
         isRecording = true
         print("Currently Recording")
         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
@@ -26,91 +53,116 @@ class SpeechRecognizerManager: ObservableObject {
         guard let recognitionRequest = recognitionRequest else { return }
         recognitionRequest.shouldReportPartialResults = true
 
+        // Create a recognition task with a result handler.
         recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { [weak self] (result, error) in
+            // Update the transcribed text as speech is recognized.
             if let result = result {
                 let transcribedText = result.bestTranscription.formattedString
+                    
                 print(transcribedText) // Print the transcribed text in real-time
             }
 
+            // Stop recording on error.
             if error != nil {
                 self?.stopRecording()
             }
         })
 
+        // Setup the audio input node and configure the audio session.
         let inputNode = audioEngine.inputNode
         inputNode.removeTap(onBus: 0)
 
-        // Configure the audio session
+        // Configure the audio session for recording.
         let audioSession = AVAudioSession.sharedInstance()
         try? audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
         try? audioSession.setActive(true, options: .notifyOthersOnDeactivation)
 
+        // Install an audio tap to capture the microphone input.
         let recordingFormat = inputNode.outputFormat(forBus: 0)
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
             recognitionRequest.append(buffer)
-            self.analyzeBuffer(buffer)
+            self.analyzeBuffer(buffer) // Analyze buffer for silence detection.
         }
 
+        // Prepare and start the audio engine.
         audioEngine.prepare()
         try? audioEngine.start()
     }
 
-    
+    // Stops the recording and cleans up.
     func stopRecording() {
+        // Stop the audio engine and invalidate the recognition request and task.
         audioEngine.stop()
         recognitionRequest?.endAudio()
         recognitionTask?.cancel()
         recognitionRequest = nil
         recognitionTask = nil
         isRecording = false
+        silenceTimer?.invalidate()
+        silenceTimer = nil
         print("Stopped Recording")
     }
     
-    
+    // Analyzes the audio buffer to detect silence.
     func analyzeBuffer(_ buffer: AVAudioPCMBuffer) {
-            let avgPower = calculateAveragePower(from: buffer)
-            //print(avgPower)
+        let avgPower = calculateAveragePower(from: buffer)
+        //print(avgPower)
 
-            if avgPower < silenceThreshold {
-                if !isSilenceDetected {
-                    startSilenceTimer()
-                    isSilenceDetected = true
-                    print("Silence has been detected")
-                }
-            } else {
-                print("Silence is now false")
-                isSilenceDetected = false
-                silenceTimer?.invalidate()
+        // Check if the average power is below the silence threshold.
+        if avgPower < silenceThreshold {
+            // If silence is not already detected, start the silence timer.
+            if !isSilenceDetected {
+                startSilenceTimer()
+                isSilenceDetected = true
+                // print("Silence has been detected")
             }
+        } else {
+            // If not silent, invalidate the timer and reset the flag.
+            isSilenceDetected = false
+            silenceTimer?.invalidate()
         }
+    }
 
+    // Starts a timer that triggers after a certain duration of silence.
     private func startSilenceTimer() {
         DispatchQueue.main.async {
             self.silenceTimer?.invalidate()  // Invalidate any existing timer
             self.silenceTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { [weak self] _ in
-                print("WORK")
-                self?.handlePauseDetected()
+                self?.handlePauseDetected() // Handle pause detection.
             }
         }
     }
 
-        private func handlePauseDetected() {
-            print("Pause detected")
-            // Handle the detected pause, e.g., stop recording or process the current recording
-        }
+    // Handles the detected pause in speech.
+    private func handlePauseDetected() {
+        print("Pause detected")
+        // Implement actions to handle the detected pause.
+        
+        //1. Save Current Recording to a playable file
+        
+        //2. Take Care of Sending File To Backend
+    }
     
+    private func sendFile(){
+        print("File Sent")
+    }
+    
+    // Calculates the average power of the audio buffer in decibels.
     private func calculateAveragePower(from buffer: AVAudioPCMBuffer) -> Float {
         guard let channelData = buffer.floatChannelData else { return 0.0 }
 
+        // Extract the frame values and calculate the RMS.
         let channelDataValue = channelData.pointee
         let frames = Int(buffer.frameLength)
         let channelDataValues = (0..<frames).map { channelDataValue[$0] }
 
         let rms = sqrt(channelDataValues.map { $0 * $0 }.reduce(0, +) / Float(frames))
 
-        // Convert to decibels
+        // Convert RMS to decibels.
         let avgPower = 20 * log10(rms)
         return avgPower
     }
+
+
 }
 
